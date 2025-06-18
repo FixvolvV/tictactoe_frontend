bd9b0d67f525526baa694a429626912c5fc5163a
FixvolvV <neskawy222@gmail.com>
2025-06-18 08:05:58 +0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import fs from 'fs';
-import path from 'path'; // <- Обратите внимание на path
 
 
 // https://vite.dev/config/
